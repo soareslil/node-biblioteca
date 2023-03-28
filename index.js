af606b6a15f8d0d7ae0b76dbe979a79e6699050c
soareslil <lilianectd@gmail.com>
2023-03-28 09:29:46 -0300

    try,catch, finaly

--- index.js
+++ index.js
@@ -2,19 +2,45 @@ import fs from 'fs';
 import chalk from 'chalk';
 
 function trataErro(erro){
-    console.log(chalk.bgGreenBright(erro))
+    console.log(chalk.yellow(erro))
     throw new Error(chalk.red(erro.code, 'nao tem arquivos'));
 }
 
-function pegaArquivo(caminhoDoArquivo){
-    const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (erro, texto)=>{
-       if(erro){
-        trataErro(erro);
-       }
-       console.log(chalk.bgMagentaBright(texto))
-    })
+async function pegaArquivo(caminhoDoArquivo){
+    try{
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        console.log(chalk.greenBright(texto))
+    } catch(erro){
+        trataErro(erro)
+    }
+    finally{
+        console.log(chalk.cyanBright('operacao finalizadaHHHHH'))
+    }
 }
 
 
-pegaArquivo('./arquivos/textos.md');
+
+
+// promises com then()
+// function pegaArquivo(caminhoDoArquivo){
+//     const encoding = 'utf-8';
+//     fs.promises
+//     .readFile(caminhoDoArquivo, encoding)
+//     .then((texto) => console.log(chalk.bgYellowBright(texto)))
+//     .catch(trataErro)
+// }
+
+
+// function pegaArquivo(caminhoDoArquivo){
+//     const encoding = 'utf-8';
+//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto)=>{
+//        if(erro){
+//         trataErro(erro);
+//        }
+//        console.log(chalk.bgMagentaBright(texto))
+//     })
+// }
+
+
+pegaArquivo('./arquivos/text.md');
