a5b9c0cf6ced3913df2ebee933d2b3b1f2200e72
soareslil <lilianectd@gmail.com>
2023-03-28 08:49:18 -0300

    tratamento de erros

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+function trataErro(erro){
+    throw new Error(chalk.red(erro.code, 'nao tem arquivos'));
+}
+
 function pegaArquivo(caminhoDoArquivo){
     const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (_, texto)=>{
-        console.log(chalk.green(texto));
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto)=>{
+       if(erro){
+        trataErro(erro);
+       }
+       console.log(chalk.bgMagentaBright(texto))
     })
 }
 
