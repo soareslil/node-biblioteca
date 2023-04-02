3e4c799f48e8360fead2bc01b88bbb4fa38c8b2a
soareslil <lilianectd@gmail.com>
2023-04-02 12:04:46 -0300

    executando comandos

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+
+import pegaArquivo from "./index.js";
+
+
+const caminho = process.argv;
+
+
+pegaArquivo(caminho[2]);
--- src/index.js
+++ src/index.js
@@ -55,5 +55,4 @@ async function pegaArquivo(caminhoDoArquivo){
 //     })
 // }
 
-
-pegaArquivo('./arquivos/texto.md');
+export default pegaArquivo;
