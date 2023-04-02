49a84f320b242761c757ec4239420a20f1625e40
soareslil <lilianectd@gmail.com>
2023-04-02 12:11:08 -0300

    refatorando

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,12 @@
-
+import chalk from "chalk";
 import pegaArquivo from "./index.js";
 
 
 const caminho = process.argv;
 
+async function processaTexto(caminho){
+    const resultado = await pegaArquivo(caminho[2]);
+    console.log(chalk.yellow('lista de links'), resultado)
+}
 
-pegaArquivo(caminho[2]);
+processaTexto(caminho)
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function extraiLinks(texto) {
     const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
     const capturas = [...texto.matchAll(regex)];
     const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
-    return resultados;
+    return resultados.length !==0 ? resultados : 'nao ha links no arquivo';
   }
 
 // const capturas = regex.exec(texto);
@@ -23,7 +23,7 @@ async function pegaArquivo(caminhoDoArquivo){
     try{
         const encoding = 'utf-8';
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-       console.log(extraiLinks(texto))
+       return extraiLinks(texto)
     } catch(erro){
         trataErro(erro)
     }
