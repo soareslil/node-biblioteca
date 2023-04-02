97b9abadb2677b551b59d58da2d983fab5c7bb88
soareslil <lilianectd@gmail.com>
2023-04-02 13:14:20 -0300

    tratando erros

--- src/cli.js
+++ src/cli.js
@@ -6,13 +6,26 @@ import pegaArquivo from "./index.js";
 
 const caminho = process.argv;
 
-function imprimeLista(resultado){
-    console.log(chalk.yellow('lista de links'), resultado)
+function imprimeLista(resultado, identificador = ''){
+    console.log(
+        chalk.yellow('lista de links'),
+        chalk.red.bgGreen(identificador),
+        resultado
+    )
     
 }
 
 async function processaTexto(argumentos){
-    const caminho = argumentos[2]
+    const caminho = argumentos[2];
+
+    try{
+        fs.lstatSync(caminho);
+    }catch(erro){
+        if(erro.code === 'ENOENT'){
+            console.log('nao existe');
+            return;
+        }
+    }
 
     if(fs.lstatSync(caminho).isFile()){
         const resultado = await pegaArquivo(argumentos[2]);
@@ -21,7 +34,7 @@ async function processaTexto(argumentos){
         const arquivos = await fs.promises.readdir(caminho)
         arquivos.forEach(async (nomeDeArquivo) => {
             const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
-            imprimeLista(lista, 'aqui lista')
+            imprimeLista(lista, nomeDeArquivo)
         });
     }
 }
